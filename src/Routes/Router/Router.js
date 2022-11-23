fc075070a5c66c6aefb6e2b5ae0f4ae841bf585c
nishadahmed5 <nishatahmed650@gmail.com>
2022-11-24 00:21:18 +0600

    first commit

---
+++ src/Routes/Router/Router.js
@@ -0,0 +1,15 @@
+import Main from "../../Layouts/Main";
+import Home from "../../Pages/Home/Home";
+
+const { createBrowserRouter } = require("react-router-dom");
+
+export const router = createBrowserRouter([
+    {
+        path: '/', element: <Main></Main>, children:[
+            {
+                path: '/', element: <Home></Home>
+            }
+        ]
+    }
+])
+  
